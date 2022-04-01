89e0fe0b85d903fcdf342857275a1bde84cccd90
y-web21 <y.web2019@gmail.com>
2022-04-01 17:22:16 +0900

    run fomatter

--- scripts/install-essential-package.bash
+++ scripts/install-essential-package.bash
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
 pkgs=(
-tree
-htop
-jq
-# yq
+  tree
+  htop
+  jq
+  # yq
 )
 optional=(
-xdg-utils, xdg-open
-source-highlight
-psmisc,'pstree. '
-nano, '素人用エディタ'
-ncdu, '使いやすいdu'
+  xdg-utils, xdg-open
+  source-highlight
+  psmisc,'pstree. '
+  nano, '素人用エディタ'
+  ncdu, '使いやすいdu'
 )
 
 for opt in "${optional[@]}"; do
-    pkg=$(echo ${opt} | awk -F "," '{print $1}')
-    msg=$(echo ${opt} | awk -F "," '{print $2}')
-    if [ "${msg}" == '' ]; then msg=$pkg; fi
-    read -p "use ${msg}? (y or else): " key
-    case "$key" in [yY] | [yY]es) pkgs+=("$pkg");; esac
+  pkg=$(echo ${opt} | awk -F "," '{print $1}')
+  msg=$(echo ${opt} | awk -F "," '{print $2}')
+  if [ "${msg}" == '' ]; then msg=$pkg; fi
+  read -p "use ${msg}? (y or else): " key
+  case "$key" in [yY] | [yY]es) pkgs+=("$pkg") ;; esac
 done
 
 PACKAGE_MANAGER=aptitude
@@ -28,33 +28,33 @@ test -f /etc/system-release && PACKAGE_MANAGER='yum'
 test -f /etc/debian_version && PACKAGE_MANAGER='apt'
 
 if [ ${EUID:-${UID}} = 0 ]; then
-    $PACKAGE_MANAGER update
+  $PACKAGE_MANAGER update
 else
-    sudo $PACKAGE_MANAGER update
+  sudo $PACKAGE_MANAGER update
 fi
 
 for pkg in "${pkgs[@]}"; do
-    # if [[ "$(which ${pkg})" == '' ]];then
-    # fi
-    $pkg --version &> /dev/null
-    if [ $? -eq 0 ] ; then
-        :
+  # if [[ "$(which ${pkg})" == '' ]];then
+  # fi
+  $pkg --version &>/dev/null
+  if [ $? -eq 0 ]; then
+    :
+  else
+    echo trying to install $pkg...
+    if [ ${EUID:-${UID}} = 0 ]; then
+      $PACKAGE_MANAGER install -y $pkg
     else
-        echo trying to install $pkg...
-        if [ ${EUID:-${UID}} = 0 ]; then
-            $PACKAGE_MANAGER install -y $pkg
-        else
-            sudo $PACKAGE_MANAGER install -y $pkg
-        fi
-        if [ $? -ne 0 ] ; then
-            faillist+=("${pkg}")
-        fi
+      sudo $PACKAGE_MANAGER install -y $pkg
     fi
+    if [ $? -ne 0 ]; then
+      faillist+=("${pkg}")
+    fi
+  fi
 done
 
 for pkg in "${faillist[@]}"; do
-    read -t 3 -p "${pkg} install failed."
-    echo ''
+  read -t 3 -p "${pkg} install failed."
+  echo ''
 done
 
 unset $faillist
