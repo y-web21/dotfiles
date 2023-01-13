7f13651bb202a47c7099963bb1569bf077a448ce
y-web21 <51986098+y-web21@users.noreply.github.com>
2023-01-13 16:29:42 +0000

    cut out to a separate file. clip and is_xxx.

---
+++ shell.d/func/is.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+is_debian_based() { [ -f /etc/debian_version ]; } # /etc/os-release ID_LIKE=debian || ID=debian
+is_redhat_based() { [ -f /etc/redhat-release ]; }
+is_slackware_based() { :; }
+# window
+is_wayland() { [ -v WAYLAND_DISPLAY ]; }
+is_xorg() { echo 1; } # X11, TBDevelopment
+# os
+is_centos() { [ "$(head </etc/redhat-release -1 | cut -d' ' -f1 2>/dev/null)" = 'CentOS' ]; }
+is_linux() { [ "$(printf '%s' "$(uname -s)" | cut -c 1-5)" = 'Linux' ]; }
+is_mac() { [ "$(uname)" = 'Darwin' ]; }
+is_ubuntu() { grep '^ID=ubuntu' /etc/os-release >/dev/null 2>&1; }
+is_debian() { grep '^ID=debian' /etc/os-release >/dev/null 2>&1; }
+# shellcheck disable=SC2317
+is_wsl() { type wslpath >/dev/null 2>&1; } # type2
+is_wsl() { [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; }
+# vm
+is_lxc_container() { sudo grep -qa container=lxc /proc/1/environ; }
+is_crostini() { is_debian && is_lxc_container; }
