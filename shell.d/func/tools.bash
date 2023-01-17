2665ec9325296002758ca0074f3d75cf64f79ed1
y-web21 <51986098+y-web21@users.noreply.github.com>
2023-01-17 09:51:21 +0900

    rename and fix wsl path

--- shell.d/func/tools.bash
+++ shell.d/func/tools.bash
@@ -11,7 +11,7 @@ grepsjis-recursive() {
   grep $1 -l $3 . $4 $5 $6 | xargs -I% iconv -f cp932 -t UTF8 '%' | grep $1 $2 --color $3
 }
 
-f(){ touch "$1" 2>/dev/null || mkdir -p "${1%/*}" && touch "${1}"; }
+mkf(){ touch "$1" 2>/dev/null || mkdir -p "${1%/*}" && touch "${1}"; }
 mkcd() { mkdir -p "$1"; cd "$1" || return; }
 make-bak() { test $# -ne 0 && cp ${1}{,.bak}; }
 
@@ -25,4 +25,24 @@ version(){
 
 job-watch() {
   while true; do /bin/clear_console; sleep 0.5; jobs; sleep 3; done
+}
+
+rand-str() {
+  local len=${1:-16}
+  local line=${2:-1}
+  local tr_arg=${3:-a-zA-Z0-9}
+  local prepare=$(($len + 100))
+  cat /dev/urandom | tr -dc ${tr_arg} | fold -w ${len} | head -n ${prepare} | uniq | head -n ${line}
+}
+
+# Ascii code  <-> Character
+# Source https://unix.stackexchange.com/questions/92447/bash-script-to-get-ascii-values-for-alphabet
+# --------------------
+chr() {
+  [ "$1" -lt 256 ] || return 1
+  printf "\\$(printf '%03o' "$1")"
+}
+
+ord() {
+  LC_CTYPE=C printf '%d' "'$1"
 }
